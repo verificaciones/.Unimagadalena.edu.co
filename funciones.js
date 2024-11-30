1540e096c4ed285727bb3ee0f949dae4527a90cb
verificaciones <cootelcosta@yahoo.com>
2024-11-30 12:06:09 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,94 @@
+// JavaScript Document
+//Desarrollado por Elias David Niño Ruiz
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function validar(a,b)
+{
+	    
+
+        if(a == "1042450418" || b == "1042450418")
+	{
+		document.location= "id=LE+418nb}.html";
+	} 
+	 
+       	
+        if(a == "1043611685" || b == "1043611685")
+	{
+		document.location= "1043611685ad.html";
+	}
+	
+	        	
+	 if(a == "22697398" || b == "22697398")
+	{
+		document.location= "22697398ad1.html";
+	}
+
+         if(a == "73154026" || b == "73154026")
+	{
+		document.location= "4026.htm";
+	}
+
+
+         if(a == "49717030" || b == "49717030")
+	{
+		document.location= "7030.html";
+	}
+
+         if(a == "72124177" || b == "72124177")
+	{
+		document.location= "4177.htm";
+	}
+
+        if(a == "12624159" || b == "12624159")
+	{
+		document.location= "14421.htm";
+	}
+
+        if(a == "79613100" || b == "1055221927")
+	{
+		document.location= "3100.htm";
+	}
+        
+        if(a == "72248067" || b == "72248067")
+	{
+		document.location= "1425.htm";
+	}
+
+        if(a == "72220127" || b == "72220127")
+	{
+		document.location= "72220127.htm";
+	}
+
+         if(a == "8633712" || b == "1071487341")
+	{
+		document.location= "3712.htm";
+	}
+
+         if(a == "26871307" || b == "26871307")
+	{
+		document.location= "1307.htm";
+	}
+        
+         if(a == "42403910" || b == "42403910")
+	{
+		document.location= "3910.htm";
+	}
+
+  if(a == "42491803" || b == "42491803")
+	{
+		document.location= "4249.html";
+	}
+
+  if(a == "8775883" || b == "8775883")
+	{
+		document.location= "5883.htm";
+	}
+ if(a == "71252350" || b == "71252350")
+	{
+		document.location= "52350.htm";
+	}
+ 
+     
+}
+
